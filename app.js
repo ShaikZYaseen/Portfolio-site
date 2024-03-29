2bccfd3c34e532db726828496e5ed2e385bcfee9
Yaseen <hackercanhack69@gmail.com>
2024-03-29 19:47:48 +0530

    second

--- app.js
+++ app.js
@@ -6,17 +6,30 @@ hamburger.onclick = function(){
     navBar.classList.toggle("active") 
     
 }
-let options = document.querySelectorAll(".activee");
-for(let option of options){
-options.onclick = function(){
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active") 
-    console.log("clicked");
 
-}
-}
 
 
 var typed = new Typed('#typed', {
     stringsElement: '#typed-strings'
   });
+
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwRtSCu0ahiDEKQjY6dYeNXYZWhRcfp80U9gymDqKHnbr9fHaxcirGWTg0gp1hzmhcI/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Successfully submitted!"
+        setTimeout(function(){
+            msg.innerHTML = " "
+            
+        },5000);
+        form.reset();
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
